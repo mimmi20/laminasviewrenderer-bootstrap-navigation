eb5fad221794dd8e526f2b39bb33865097d423a6
Thomas Müller <mimmi20@live.de>
2024-05-19 12:49:59 +0200

    fix loading error

---
+++ tests/HelperPluginManagerFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This file is part of the mimmi20/laminasviewrenderer-bootstrap-navigation package.
+ *
+ * Copyright (c) 2021-2024, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Mimmi20Test\LaminasView\BootstrapNavigation;
+
+use Laminas\View\HelperPluginManager;
+use Mimmi20\LaminasView\BootstrapNavigation\HelperPluginManagerFactory;
+use PHPUnit\Framework\Exception;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+
+final class HelperPluginManagerFactoryTest extends TestCase
+{
+    private HelperPluginManagerFactory $factory;
+
+    /** @throws void */
+    protected function setUp(): void
+    {
+        $this->factory = new HelperPluginManagerFactory();
+    }
+
+    /**
+     * @throws Exception
+     * @throws ContainerExceptionInterface
+     */
+    public function testInvocation(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::once())
+            ->method('get')
+            ->with('config')
+            ->willReturn([]);
+        $container->expects(self::never())
+            ->method('has');
+
+        $helper = ($this->factory)($container, '');
+
+        self::assertInstanceOf(HelperPluginManager::class, $helper);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ContainerExceptionInterface
+     */
+    public function testInvocation2(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::once())
+            ->method('get')
+            ->with('config')
+            ->willReturn(['view_helpers' => []]);
+        $container->expects(self::never())
+            ->method('has');
+
+        $helper = ($this->factory)($container, '');
+
+        self::assertInstanceOf(HelperPluginManager::class, $helper);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ContainerExceptionInterface
+     */
+    public function testInvocation3(): void
+    {
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $container->expects(self::once())
+            ->method('get')
+            ->with('config')
+            ->willReturn(['view_helpers' => ['']]);
+        $container->expects(self::never())
+            ->method('has');
+
+        $helper = ($this->factory)($container, '');
+
+        self::assertInstanceOf(HelperPluginManager::class, $helper);
+    }
+}
